a12bd69f0ab58cc5fb80c6cb132c565e9a577d23
Aoi Kondo <agotadmidori@gmail.com>
2024-11-02 18:36:13 +0900

    wip: Successfully recognize initialize files

--- internal/pkg/container/up.go
+++ internal/pkg/container/up.go
@@ -5,18 +5,24 @@ import (
 	"bytes"
 	"context"
 	"io"
+	"io/fs"
 	"log"
 	"log/slog"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/mount"
 	"github.com/docker/docker/client"
+
+	"github.com/facebookgo/symwalk"
 )
 
 var DOCKERFILE_NAME string = "Dockerfile.cuda-11.1.1"
 
-func ContainerCreateAndStart() int {
+func ContainerCreateAndStart(HostVolumeAbsolutePath string) int {
 	buildDockerfile("sample")
 
 	ctx := context.Background()
@@ -32,14 +38,6 @@ func ContainerCreateAndStart() int {
 	// If need environmental value, Please write below.
 	envvalue1 := "ENV_VALUE=TEST"
 
-	/*
-		absolutePath, err := filepath.Abs("../docker/images/workspace/")
-		if err != nil {
-			panic(err)
-		}
-		sourcepath := absolutePath + "/"
-	*/
-
 	resp, err := cli.ContainerCreate(
 		ctx,
 		&container.Config{
@@ -47,7 +45,15 @@ func ContainerCreateAndStart() int {
 			Cmd:   []string{},
 			Env:   []string{envvalue1},
 			Tty:   false,
-		}, nil, nil, nil, "")
+		}, &container.HostConfig{
+			Mounts: []mount.Mount{
+				{
+					Type:   mount.TypeBind,
+					Source: HostVolumeAbsolutePath,
+					Target: "/workspace",
+				},
+			},
+		}, nil, nil, "")
 	if err != nil {
 		panic(err)
 	}
@@ -66,10 +72,12 @@ func ContainerCreateAndStart() int {
 
 	case exitCode = <-exitCodeCh:
 		// End Process - Remove container
-		err = cli.ContainerRemove(ctx, resp.ID, container.RemoveOptions{})
-		if err != nil {
-			panic(err)
-		}
+		/*
+			err = cli.ContainerRemove(ctx, resp.ID, container.RemoveOptions{})
+			if err != nil {
+				panic(err)
+			}
+		*/
 
 		//WA
 		if exitCode.StatusCode == 1 {
@@ -109,7 +117,7 @@ func buildDockerfile(jobname string) {
 	}
 	res, err := cli.ImageBuild(
 		ctx,
-		getArchivedDockerfile(DOCKERFILE_NAME),
+		getArchivedContextdirectory("../test"),
 		buildOptions,
 	)
 	if err != nil {
@@ -120,39 +128,61 @@ func buildDockerfile(jobname string) {
 	io.Copy(os.Stdout, res.Body)
 }
 
-func getArchivedDockerfile(dockerfile string) *bytes.Reader {
-	// read the Dockerfile
-
-	filepath := "../docker/images/" + dockerfile
-	f, err := os.Open(filepath)
-	if err != nil {
-		log.Panic(err)
-	}
-	defer func() {
-		if err := f.Close(); err != nil {
-			log.Panic(err)
-		}
-	}()
-	b, err := io.ReadAll(f)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	// archive the Dockerfile
-	tarHeader := &tar.Header{
-		Name: dockerfile,
-		Size: int64(len(b)),
-	}
+func getArchivedContextdirectory(contextDir string) *bytes.Reader {
 	buf := new(bytes.Buffer)
+
 	tw := tar.NewWriter(buf)
 	defer tw.Close()
-	err = tw.WriteHeader(tarHeader)
-	if err != nil {
-		log.Panic(err)
-	}
-	_, err = tw.Write(b)
-	if err != nil {
-		log.Panic(err)
+
+	if err := symwalk.Walk(contextDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		intake_path := strings.Replace(path, contextDir, "", 1)
+		tar_mode := info.Mode()
+		tar_size := info.Size()
+
+		if info.Mode()&fs.ModeSymlink != 0 {
+			path_sym, err := filepath.EvalSymlinks(path)
+			if err != nil {
+				return err
+			}
+			path = path_sym
+			stat, err := os.Stat(path_sym)
+			if err != nil {
+				return err
+			}
+			tar_mode = stat.Mode()
+			tar_size = stat.Size()
+		}
+
+		if err := tw.WriteHeader(&tar.Header{
+			Name:    intake_path,
+			Mode:    int64(tar_mode),
+			ModTime: info.ModTime(),
+			Size:    tar_size,
+		}); err != nil {
+			return err
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		if _, err := io.Copy(tw, f); err != nil {
+			return err
+		}
+		return nil
+
+	}); err != nil {
+		panic(err)
 	}
 
 	return bytes.NewReader(buf.Bytes())
